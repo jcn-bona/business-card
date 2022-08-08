5bb9e7835ac6599dfa050f93ed2b78b024b4ee22
Jeronimo Cardoso Neto <jeronimo.cardoso.neto@gmail.com>
2022-08-08 09:18:19 -0300

    first commit

---
+++ app/src/main/java/com/jcn/businesscard/App.kt
@@ -0,0 +1,6 @@
+package com.jcn.businesscard
+
+import android.app.Application
+
+class App : Application() {
+}
---
+++ app/src/main/java/com/jcn/businesscard/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jcn.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jcn.businesscard.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
